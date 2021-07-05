4a31c55a81ec7e9f7aaa2ca1789edf7e1055820b
Ali <salimian.ali98@gmail.com>
2021-07-05 13:30:19 +0430

    projective transform. project video into 2D_field. 4 point calculated by hand.

--- part1.py
+++ part1.py
@@ -3,10 +3,33 @@
 import numpy as np
 import cv2
 import time
+
+
+# reading fiel img:
+F = cv2.imread('../2D_field.png')
+cv2.imshow('field', F)
+
 # create a VideoCapture object
 cap = cv2.VideoCapture('../output.mp4')
 fps = cap.get(cv2.CAP_PROP_FPS)
 mspf = round(1000/fps)
+# w = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))  # width of the frame
+# h = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT)) # height of the frame
+
+# 4 points in the 2D_field.png
+points1 = np.array([(156, 151),
+                    (525, 0),
+                    (525, 700),
+                    (1050-156, 151)]).astype(np.float32)
+# 4 points in the output.mp4
+points2 = np.array([(156, 168),
+                    (686, 107),
+                    (926, 792),
+                    (1202, 117)]).astype(np.float32)
+
+# compute homography from point correspondences
+H = cv2.getPerspectiveTransform(points2, points1)
+output_size = (F.shape[1], F.shape[0])
 
 num_frames = 0
 t = time.time()
@@ -18,10 +41,15 @@ while True:
     if ret == False: # end of video (perhaps)
         break
     num_frames += 1
-    # Display I
-    cv2.imshow('win1',I)
+
+    # project the video to the field coordinate.
+    J = cv2.warpPerspective(I, H, output_size)
     
-    key = cv2.waitKey(mspf) 
+    # Display I, J
+    cv2.imshow('win1', I)
+    cv2.imshow('win2', J)
+
+    key = cv2.waitKey(mspf//3) 
 
     if key & 0xFF == ord('q'): 
         break
