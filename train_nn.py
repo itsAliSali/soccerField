86ccac00936acba249d72d8da0ad8694c23f5e5a
itsAliSali <salimian.ali98@gmail.com>
2021-07-11 12:48:08 +0430

    tune training phase + refactor

--- train_nn.py
+++ train_nn.py
@@ -1,23 +1,20 @@
 import torch
-import torchvision
 import torchvision.transforms as transforms
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
 
 import my_data as ds
 
-import torch.nn as nn
-import torch.nn.functional as F
-
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-# print(">>>>> ", device)
-
 
 class Net(nn.Module):
     def __init__(self):
         super().__init__()
         self.conv1 = nn.Conv2d(3, 6, 5)
         self.pool = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(6, 16, 5)
-        self.fc1 = nn.Linear(3168, 520)
+        self.conv2 = nn.Conv2d(6, 8, 5)
+        self.fc1 = nn.Linear(8 * 9 * 22, 520)
         self.fc2 = nn.Linear(520, 40)
         self.fc3 = nn.Linear(40, 1)
 
@@ -26,7 +23,7 @@ class Net(nn.Module):
         x = self.pool(F.relu(self.conv2(x))).to(device)
         x = torch.flatten(x, 1).to(device) # flatten all dimensions except batch
         x = F.relu(self.fc1(x)).to(device)
-        x = torch.sigmoid(self.fc2(x)).to(device)
+        x = F.relu(self.fc2(x)).to(device)
         x = torch.sigmoid(self.fc3(x).to(device))
         return x
 
@@ -34,41 +31,36 @@ class Net(nn.Module):
 transform = transforms.Compose([transforms.ToTensor()])#, transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 
 
-if __name__ == "__main__":
-   
+if __name__ == "__main__":   
 
     batch_size = 10
 
     trainset = ds.SoccerFieldDataset('./data/dataset/train/', transform=transform)
-
     trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size,
                                             shuffle=True, num_workers=2)
 
     testset = ds.SoccerFieldDataset('./data/dataset/test/', transform=transform)
-    testloader = torch.utils.data.DataLoader(testset, batch_size=1,
+    testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size,
                                             shuffle=False, num_workers=2)
-
 
     net = Net()
     net = net.double()
     net.to(device)
 
-    import torch.optim as optim
-
-    # criterion = nn.CrossEntropyLoss()
     criterion = nn.MSELoss()
     optimizer = optim.SGD(net.parameters(), lr=0.1, momentum=0.05)
 
-
-    for epoch in range(10):  # loop over the dataset multiple times
-
+    for epoch in range(10): 
+        print("#"*10, f'epoch {epoch+1}', "#"*10)
+        
         # training
+        net.train()
         loss_train = 0.0
         running_loss = 0.0
+        num = 0
         for i, data in enumerate(trainloader, 0):
-            # get the inputs; data is a list of [inputs, labels]
+            
             inputs, labels = data
-            # inputs = inputs.permute(0, 3, 1, 2).to(device)
             inputs = inputs.to(device)
             labels = labels.reshape((-1,1)).double().to(device)
         
@@ -81,6 +73,10 @@ if __name__ == "__main__":
             loss.backward()
             optimizer.step()
 
+            # accuracy
+            corrects = (outputs > 0.5) == labels
+            num += sum(corrects)
+            
             # print statistics
             running_loss += loss.item()
             loss_train += loss.item()
@@ -89,34 +85,31 @@ if __name__ == "__main__":
                     (epoch + 1, i + 1, running_loss / 100 ))
                 running_loss = 0.0
 
-        print('>>>> epoch: %d, train_loss: %.6f' %
-                    (epoch + 1, loss_train / trainloader.__len__() ))
-        
+        # print('>>>> epoch: %d, train_loss: %.6f' %
+        #             (epoch + 1, loss_train / trainset.__len__() ))
+        print(f"train_acc = {float(num/trainset.__len__() * 100):.4f}")
 
         # test
+        net.eval()
         loss_test = 0 
         num = 0
         for i, data in enumerate(testloader, 0):
-            # get the inputs; data is a list of [inputs, labels]
+            
             inputs, labels = data
-            # print(inputs.shape)
-            # inputs = inputs.permute(0, 3, 1, 2).to(device)
             inputs = inputs.to(device)
             labels = labels.reshape((-1,1)).double().to(device)
             
             # forward 
             outputs = net(inputs)
-            if outputs > 0.5 and labels == 1:
-                num += 1
-            if outputs < 0.5 and labels == 0:
-                num += 1
+            
+            # accuracy
+            corrects = (outputs > 0.5) == labels
+            num += sum(corrects)
             
             loss = criterion(outputs, labels)
             loss_test += loss.item()
         
-        print('>>>> epoch: %d, test _loss: %.6f' %
-                    (epoch + 1, loss_test / testloader.__len__() ))
-        print(f"acc= {num/testloader.__len__() *100 }")
+        print(f"test_acc = {float(num/testset.__len__() * 100):.4f}\n")
         
 
     from datetime import datetime as dt
@@ -124,37 +117,3 @@ if __name__ == "__main__":
 
     print('Finished Training')
     torch.cuda.empty_cache()
-
-
-    # import random 
-    # import matplotlib.pyplot as plt
-    # import cv2
-    # import numpy as np
-    # dss = ds.SoccerFieldDataset('./data/dataset/train/', transform)
-
-    # print(dss.__len__())
-
-    # # d0, l0 = ds.__getitem__(0)
-    # # d1, l1 = ds.__getitem__(175)
-    # # d2, l2 = ds.__getitem__(187)
-
-    # f, ax = plt.subplots(4,5)
-
-    # for i in range(20):
-    #     idx = random.randint(0, dss.__len__())
-    #     img, lbl = dss.__getitem__(idx)
-    #     imgo = img.cpu().permute(1,2,0).numpy()
-    #     ##
-    #     # img = cv2.resize(img, (50, 100))
-    #     # img = np.float64(img/255)
-    #     # img = transform(img)
-    #     img = img.reshape(1, 3, 100, 50).double().to(device)
-    #     # img = img.permute(0, 3, 1, 2).double().to(device)
-    #     output = net(img)
-    # ##
-    #     ax[i//5,i%5].imshow(imgo)
-    #     ax[i//5,i%5].title.set_text(f'{lbl}/{float(output)}')
-    #     ax[i//5,i%5].axis('off')
-    
-    # plt.show()
-    # torch.cuda.empty_cache()
